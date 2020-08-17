f4bd344976e8305cb9e0bb0dc1d4d23168b5290f
Ivan Volodartsev <untrupus@gmail.com>
2020-08-17 12:52:48 +0600

    add something new

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,24 +7,10 @@ import axios from 'axios';
 function App() {
     const [messenger, setMessenger] = useState([]);
     const [myMessage, setMyMessage] = useState({author: '', message: ''});
-    const [post, setPost] = useState({author: '', message: ''});
 
     const getMessages = async () => {
         const result = await axios('http://146.185.154.90:8000/messages');
         setMessenger(result.data);
-    }
-
-    const postMessage = async () => {
-        const url = 'http://146.185.154.90:8000/messages';
-        const data = new URLSearchParams();
-        if (post.message !== '' && post.author !== '') {
-            data.set('message', post.message);
-            data.set('author', post.author);
-            await fetch(url, {
-                method: 'post',
-                body: data,
-            });
-        }
     }
 
     useEffect(() => {
@@ -33,10 +19,6 @@ function App() {
         }, 2000);
     }, []);
 
-    useEffect(() => {
-        postMessage().catch(console.error);
-    }, [post]);
-
     const newMessage = event => {
         const attr = event.target.name;
         const newField = {...myMessage};
@@ -44,8 +26,17 @@ function App() {
         setMyMessage(newField);
     }
 
-    const changePost = () => {
-        setPost(myMessage)
+    const changePost = async () => {
+        const url = 'http://146.185.154.90:8000/messages';
+        const data = new URLSearchParams();
+        if (myMessage.message !== '' && myMessage.author !== '') {
+            data.set('message', myMessage.message);
+            data.set('author', myMessage.author);
+            await fetch(url, {
+                method: 'post',
+                body: data,
+            });
+        }
         setMyMessage({author: '', message: ''})
     }
 
